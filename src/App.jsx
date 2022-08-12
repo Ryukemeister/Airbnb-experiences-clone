6e49dc153f20545ce79dcadf43eb7f062c426619
Ryuke <sahalrajiv-extc@atharvacoe.ac.in>
2022-08-12 21:04:52 +0530

    Added a card component section

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,31 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
+import Navbar from "./components/Navbar";
+import Main from "./components/Main";
+import Card from "./components/Card";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <div className="App">
-      <h1>Hi</h1>
+      <Navbar />
+      <Main />
+      <div className="cards-deck">
+        <Card />
+        <Card
+          reviews="30"
+          intro="Learn wedding photography"
+          price="125"
+          img="./src/assets/Wedding.png"
+        />
+        <Card
+          reviews="2"
+          rating="4.8"
+          intro="Group mountain biking"
+          price="50"
+          img="./src/assets/Cycyling.png"
+        />
+      </div>
     </div>
   );
 }
