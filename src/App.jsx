bfa25f85eb2f692c111f0c0b965b13daa4b8beef
Ryuke <sahalrajiv-extc@atharvacoe.ac.in>
2022-08-15 21:11:28 +0530

    Added some conditional rendering and a mapping method for our card component

--- src/App.jsx
+++ src/App.jsx
@@ -3,58 +3,30 @@ import reactLogo from "./assets/react.svg";
 import Navbar from "./components/Navbar";
 import Main from "./components/Main";
 import Card from "./components/Card";
+import data from "./components/data";
 import "./App.css";
 
 function App() {
+  const sliderComponents = data.map((item) => {
+    return (
+      <Card
+        key={item.id}
+        img={`./src/assets/${item.coverImg}`}
+        rating={item.stats.rating}
+        reviews={item.stats.reviewCount}
+        location={item.location}
+        intro={item.title}
+        price={item.price}
+        openSpots={item.openSpots}
+      />
+    );
+  });
+
   return (
     <div className="App">
       <Navbar />
       <Main />
-      <div className="cards-deck">
-        <Card />
-        <Card
-          reviews="30"
-          intro="Learn wedding photography"
-          price="125"
-          img="./src/assets/Wedding.png"
-        />
-        <Card
-          reviews="2"
-          rating="4.8"
-          intro="Group mountain biking"
-          price="50"
-          img="./src/assets/Cycyling.png"
-        />
-        <Card />
-        <Card
-          reviews="30"
-          intro="Learn wedding photography"
-          price="125"
-          img="./src/assets/Wedding.png"
-        />
-        <Card
-          reviews="2"
-          rating="4.8"
-          intro="Group mountain biking"
-          price="50"
-          img="./src/assets/Cycyling.png"
-        />
-        <Card />
-        <Card
-          reviews="30"
-          intro="Learn wedding photography"
-          price="125"
-          img="./src/assets/Wedding.png"
-        />
-        <Card
-          reviews="2"
-          rating="4.8"
-          intro="Group mountain biking"
-          price="50"
-          img="./src/assets/Cycyling.png"
-        />
-        <Card />
-      </div>
+      <section className="cards-section"> {sliderComponents}</section>
     </div>
   );
 }
