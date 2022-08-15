d0e91052239fdc76f88e87e5d2c83b16bfa457b0
Ryuke <sahalrajiv-extc@atharvacoe.ac.in>
2022-08-15 21:13:28 +0530

    Added some more props for the card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,35 +1,26 @@
 import React from "react";
 import "./Card.css";
 
-/*
-function Card() {
-  return (
-    <div className="card-container">
-      <img className="katie-picture" src="./src/assets/Katie.png"></img>
-      <div className="card-stats">
-        <img className="star-rating" src="./src/assets/Star 1.png"></img>
-        <span>5.0</span>
-        <span className="gray">(6) • </span>
-        <span className="gray">USA</span>
-      </div>
-      <p>Life Lessons with Katie Zaferes</p>
-      <p>
-        <span className="bold">From $136</span> / person
-      </p>
-    </div>
-  );
-}
-*/
+// {openSpots === 0 ? <div className="card--badge">SOLD OUT</div> : ""}
+// {openSpots === 0 && <div className="card--badge">SOLD OUT</div>}
 
-function Card({ img, rating, reviews, country, intro, price }) {
+function Card({ img, rating, reviews, location, intro, price, openSpots }) {
+  let badgeText;
+  if (openSpots === 0) {
+    badgeText = "SOLD OUT";
+  } else if (location === "Online") {
+    badgeText = "ONLINE";
+  }
+
   return (
     <div className="card-container">
+      {badgeText && <div className="card--badge">{badgeText}</div>}
       <img className="katie-picture" src={img}></img>
       <div className="card-stats">
         <img className="star-rating" src="./src/assets/Star 1.png"></img>
         <span>{rating}</span>
         <span className="gray">({reviews}) • </span>
-        <span className="gray">{country}</span>
+        <span className="gray">{location}</span>
       </div>
       <p>{intro}</p>
       <p>
@@ -38,14 +29,5 @@ function Card({ img, rating, reviews, country, intro, price }) {
     </div>
   );
 }
-
-Card.defaultProps = {
-  img: "./src/assets/Katie.png",
-  rating: "5.0",
-  reviews: "6",
-  country: "USA",
-  intro: "Life Lessons with Katie Zaferes",
-  price: "136",
-};
 
 export default Card;
